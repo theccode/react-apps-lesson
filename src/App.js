6104845547315c93df87e2f232eb7e233eb88968
Eric Kumah <theccode@Erics-MacBook-Pro.local>
2020-12-27 19:15:55 +0000

    completed todo application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import {TodoBanner} from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+import { VisibilityControl } from "./VisibilityControl";
 
 export default class App extends Component {
   constructor(props){
@@ -11,7 +15,8 @@ export default class App extends Component {
         {action: "Collect Tickets", done: true},
         {action: "Call Annie", done: false}
       ],
-      newItemText: ""
+      // newItemText: ""
+      showCompleted: true
     }
   }
 
@@ -21,16 +26,27 @@ export default class App extends Component {
     })
   }
 
-  createNewTodo = () => {
-    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)){
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)){
       this.setState({
         todoItems: [
           ...this.state.todoItems, 
-          {action: this.state.newItemText, done: false}
+          {action: task, done: false}
         ],
         newItemText: ""
-      })
+      }, () => localStorage.setItem("todo", JSON.stringify(this.state)))
     }
+  }
+  componentDidMount = () => {
+    let data = localStorage.getItem("todo");
+    this.setState(data != null ? JSON.parse(data): {userName: "Naa55", todoItems: [
+      {action: "Buy Flowers", done: false},
+        {action: "Get Shoes", done: false},
+        {action: "Collect Tickets", done: true},
+        {action: "Call Annie", done: false}
+    ], 
+    showCompleted: true
+  })
   }
    changeStateData = () => {
     this.setState({
@@ -38,18 +54,45 @@ export default class App extends Component {
     })
   }
 
+  toggleTodo = (todo) => this.setState({todoItems:
+    this.state.todoItems.map(item => item.action === todo.action ? {...item, done: !item.done}: item) 
+  });
+  todoTableRows = (doneValue) =>this.state.todoItems.filter(item => item.done === doneValue).map(item =>
+    <TodoRow key={item.action} item={item} callback={ this.toggleTodo } />
+    )
   render() { 
     return (
-      <div className="container-fluid">
-        <h4 className="bg-primary text-white text-center p-2">
-          {this.state.todoItems.filter(item => !item.done).length} items to do
-        </h4>
-          <button className="btn btn-primary m-2" onClick={this.changeStateData}>
-            Change
-          </button>
-          <div className="my-1">
-            <input className="form-control" value={this.state.newItemText} onChange={this.updateNewTextValue}/>
-            <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>Add</button>
+      <div>
+          <TodoBanner name={ this.state.userName } tasks={ this.state.todoItems } />
+          <div className="container-fluid">
+            <TodoCreator callback={ this.createNewTodo } />
+              <table className="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>Description</th>
+                    <th>Done</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.todoTableRows(false)}
+                </tbody>
+              </table>
+              <div className="bg-secondary text-white text-center p-2">
+                <VisibilityControl description="Completed Tasks" isChecked={ this.state.showCompleted } 
+                  callback={ (checked) => this.setState({ showCompleted: checked })} />
+              </div>
+              { 
+              this.state.showCompleted && <table className="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Description</th>
+                      <th>Done</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.todoTableRows(true)}
+                  </tbody>
+                </table>}
           </div>
       </div>
   );
